8439fae6dcd9b7eaee2054202d715ffc405f5577
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2024-10-03 12:33:13 +0100

    customer service

---
+++ services/customer-server/src/test/java/com/devshaks/delivery/CustomerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devshaks.delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
