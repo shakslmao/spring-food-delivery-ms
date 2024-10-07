5d82f4b659afacd194078fd59254660510e30293
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2024-10-07 22:52:58 +0100

    restaurant entites

---
+++ services/restaurant-server/src/main/java/com/devshaks/delivery/restaurant/CuisineTypes.java
@@ -0,0 +1,31 @@
+package com.devshaks.delivery.restaurant;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@Table(name = "cuisine_types")
+public class CuisineTypes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private Double price;
+
+    @ManyToOne
+    @JoinColumn(name = "restaurant_id", nullable = false)
+    private Restaurant restaurant;
+}
---
+++ services/restaurant-server/src/main/java/com/devshaks/delivery/restaurant/Restaurant.java
@@ -0,0 +1,52 @@
+package com.devshaks.delivery.restaurant;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Table(name = "restaurants")
+public class Restaurant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String contactNumber;
+
+    @Column(nullable = false)
+    private String location;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private String openingHours;  // Time format: e.g., "09:00 - 21:00"
+
+    @Column(nullable = false)
+    private Double rating;
+
+    @Column(nullable = false)
+    private Boolean isOpen;
+
+    @Column(nullable = false)
+    private String priceRange;
+
+    // One-to-many relationship with CuisineTypes
+    @OneToMany(mappedBy = "restaurant", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<CuisineTypes> cuisineTypes = new ArrayList<>();
+}
